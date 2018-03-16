9f71ca68b5ea4b01594d47f481134e586387b021
Vytautas P <vytautas.pumputis@labbis.lt>
2018-03-16 08:27:08 +0200

    Add project files.

---
+++ WindowsFormsProgramming/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsProgramming
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Version ver = new Version(Application.ProductVersion);
+            Text = String.Format("My photos. Version {0:#}.{1:#}", ver.Major, ver.Minor);
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = ".JPG(.jpg)|*.jpg|.PNG(.png)|*.png|All files(*.*)|*.*";
+            dlg.CheckFileExists = true;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pbxPhoto.Image = new Bitmap(dlg.FileName);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("Unable to load file: " + exception.Message);
+                }
+                finally
+                {
+                    dlg.Dispose();
+                }
+            }
+        }
+    }
+}
