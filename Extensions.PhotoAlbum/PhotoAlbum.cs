f0923f972b1f1cb57531da95f8fd420e8a7f6bc4
Vytautas P <vytautas.pumputis@labbis.lt>
2018-03-28 14:28:19 +0300

    Implemented PhotoAlbum and Photograph classes

--- Extensions.PhotoAlbum/PhotoAlbum.cs
+++ Extensions.PhotoAlbum/PhotoAlbum.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,154 @@ namespace Extensions.PhotoAlbum
     /// <summary>
     /// A class that represents collection of Photographs
     /// </summary>
-    public class PhotoAlbum
+    public class PhotoAlbum : CollectionBase, IDisposable
     {
+        #region Fields
+        /// <summary>
+        /// tracks the current position when displaying the album
+        /// </summary>
+        private int _currentPos;
+
+        private string _fileName;
+
+        private bool _disposing = false;
+
+        #endregion
         public PhotoAlbum()
         {
             //Nothing to do
+        }
+
+        #region Properties
+
+        public string FileName
+        {
+            get { return _fileName; }
+            set { _fileName = value; }
+        }
+        public int CurrentPosition
+        {
+            get { return _currentPos; }
+            set
+            {
+                if (value <= 0)
+                    _currentPos = 0;
+                else if (value >= this.Count)
+                    _currentPos = this.Count - 1;
+                else
+                    _currentPos = value;
+            }
+        }
+
+        public Photograph CurrentPhotograph
+        {
+            get
+            {
+                if (this.Count == 0)
+                    return null;
+
+                return this[CurrentPosition];
+            }
+        }
+
+        public virtual bool IsSynchronized => false;
+
+        public virtual object SyncRoot => List.SyncRoot;
+
+        public virtual bool IsFixedSize => false;
+
+        public virtual bool IsReadOnly => false;
+
+        public virtual Photograph this[int index]
+        {
+            get { return (Photograph) List[index]; }
+            set { List[index] = value; }
+        }
+
+        #endregion
+
+        #region Methods
+        public bool CurrentNext()
+        {
+
+            if (CurrentPosition + 1 < this.Count)
+            {
+                CurrentPosition++;
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public bool CurrentPrev()
+        {
+            if (CurrentPosition > 0)
+            {
+                CurrentPosition--;
+                return true;
+            }
+
+            return false;
+        }
+
+        public virtual void CopyTo(Photograph[] array, int index)
+        {
+            List.CopyTo(array, index);
+        }
+
+        public virtual bool Contains(Photograph photo)
+        {
+            return List.Contains(photo);
+        }
+
+        public virtual int IndexOf(Photograph photo)
+        {
+            return List.IndexOf(photo);
+        }
+
+        public virtual void Insert(int index, Photograph photo)
+        {
+            List.Insert(index, photo);
+        }
+
+        public virtual void Remove(Photograph photo)
+        {
+            List.Remove(photo);
+        }
+
+        #endregion
+
+        #region Overrides
+
+        protected override void OnClear()
+        {
+            _currentPos = 0;
+            _fileName = null;
+            Dispose();
+            base.OnClear();
+        }
+
+        protected override void OnRemoveComplete(int index, object value)
+        {
+            CurrentPosition = _currentPos;
+            base.OnRemoveComplete(index, value);
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            if (!_disposing)
+            {
+                _disposing = true;
+                foreach (Photograph photograph in this)
+                {
+                    photograph.Dispose();;
+                }
+
+                Clear();
+            }
         }
     }
 }
--- Extensions.PhotoAlbum/Photograph.cs
+++ Extensions.PhotoAlbum/Photograph.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,110 @@ namespace Extensions.PhotoAlbum
     /// A class that represents single Photograps
     /// and its properties
     /// </summary>
-    public class Photograph
+    public class Photograph: IDisposable
     {
+        #region Fields
+
+        private static Bitmap _invalidImageBitmap;
+        private string _fileName;
+        private Bitmap _bitmap;
+
+        #endregion
+
+        #region Constructors
+
+        public Photograph()
+        {
+            
+        }
+
+        #endregion
+
+        #region Properties
+
+        public static Bitmap InvalidImagePhoto
+        {
+            get
+            {
+                if (_invalidImageBitmap == null)
+                {
+                    //dummy photo
+                    Bitmap bm = new Bitmap(100,100);
+                    Graphics g = Graphics.FromImage(bm);
+                    g.Clear(Color.DarkGray);
+
+                    Pen p = new Pen(Color.Red, 5);
+                    g.DrawLine(p, 0, 0, 100, 100);
+                    g.DrawLine(p, 100, 0, 0, 100);
+
+                    _invalidImageBitmap = bm;
+                }
+
+                return _invalidImageBitmap;
+            }
+        }
+        public string FileName => _fileName;
+
+        public Bitmap Image
+        {
+            get
+            {
+                if (_bitmap == null)
+                {
+                    try
+                    {
+                        _bitmap = new Bitmap(_fileName);
+                    }
+                    catch
+                    {
+                        _bitmap = InvalidImagePhoto;
+                    }
+                }
+
+                return _bitmap;
+            }
+        }
+
+        public bool IsImageValid => _bitmap != InvalidImagePhoto;
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Overrides
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Photograph)
+            {
+                Photograph p = (Photograph) obj;
+                return _fileName.ToLower().Equals(p.FileName.ToLower());
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return FileName.GetHashCode();
+        }
+
+        public void Dispose()
+        {
+            if (_bitmap != null && _bitmap != InvalidImagePhoto)
+            {
+                _bitmap.Dispose();
+            }
+
+            _bitmap = null;
+        }
+
+        public override string ToString()
+        {
+            return FileName;
+        }
+
+        #endregion
     }
 }
